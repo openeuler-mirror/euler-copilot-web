6419351449b20514237e0acba09b3ccf82c8ad49
Hongyu Shi <shywzt@iCloud.com>
2024-08-09 17:47:49 +0800

    initial system tray & shortcut

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,13 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::TrayIconBuilder;
+use tauri::{Manager, WindowEvent};
+
+#[cfg(desktop)]
+use tauri_plugin_global_shortcut::{Code, GlobalShortcutExt, Modifiers, Shortcut};
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -9,7 +16,68 @@ fn greet(name: &str) -> String {
 
 fn main() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_shell::init())
+        .setup(|app| {
+            let quit = MenuItemBuilder::new("Quit").id("quit").build(app).unwrap();
+            let hide = MenuItemBuilder::new("Hide").id("hide").build(app).unwrap();
+            let show = MenuItemBuilder::new("Show").id("show").build(app).unwrap();
+            // we could opt handle an error case better than calling unwrap
+            let menu = MenuBuilder::new(app)
+                .items(&[&quit, &hide, &show])
+                .build()
+                .unwrap();
+
+            let window = app.get_webview_window("main").unwrap();
+            let window_hider = window.clone();
+            window.on_window_event(move |event| match event {
+                WindowEvent::Focused(false) => {
+                    window_hider.hide().unwrap()
+                }
+                _ => {}
+            });
+
+            let _ = TrayIconBuilder::new()
+                .icon(app.default_window_icon().unwrap().clone())
+                .menu(&menu)
+                .on_menu_event(|app, event| match event.id().as_ref() {
+                    "quit" => app.exit(0),
+                    "hide" => {
+                        dbg!("menu item hide clicked");
+                        let window = app.get_webview_window("main").unwrap();
+                        window.hide().unwrap();
+                    }
+                    "show" => {
+                        dbg!("menu item show clicked");
+                        let window = app.get_webview_window("main").unwrap();
+                        window.show().unwrap();
+                        window.set_focus().unwrap();
+                    }
+                    _ => {}
+                })
+                .build(app);
+
+            #[cfg(desktop)]
+            {
+                let ctrl_o_shortcut = Shortcut::new(Some(Modifiers::CONTROL), Code::KeyO);
+                app.handle().plugin(
+                    tauri_plugin_global_shortcut::Builder::new().with_handler(
+                    move |app, shortcut, _event| {
+                    println!("{:?}", shortcut);
+                        if shortcut == &ctrl_o_shortcut {
+                            dbg!("Ctrl-O Detected!");
+                            let window = app.get_webview_window("main").unwrap();
+                            window.show().unwrap();
+                            window.set_focus().unwrap();
+                        }
+                    })
+                    .build(),
+                )?;
+                app.global_shortcut().register(ctrl_o_shortcut)?;
+            }
+
+            Ok(())
+        })
+        // .plugin(tauri_plugin_global_shortcut::Builder::new().build())
+        // .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
