27cef3c419fc6249603e5f24292642b08f5adbeb
cc500 <2014434568@qq.com>
2025-01-21 09:35:23 +0800

    语义接口和首页api

--- src/apis/paths/api.ts
+++ src/apis/paths/api.ts
@@ -7,7 +7,7 @@
 // IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
 // PURPOSE.
 // See the Mulan PSL v2 for more details.
-import { get, post } from 'src/apis/server';
+import { get, post, del, put } from 'src/apis/server';
 import type { FcResponse } from 'src/apis/server';
 import { ApiMessage, Service } from './type';
 
@@ -26,7 +26,7 @@ export const getApiMessageByServiceId = (
       | FcResponse<{
         serviceId: string;
         name: string;
-        apis:ApiMessage[];
+        apis: ApiMessage[];
         }>
       | undefined
     )
@@ -36,7 +36,7 @@ export const getApiMessageByServiceId = (
 };
 
 /**
- * USER登录
+ * 查询语义服务列表
  * @returns
  */
 export const getApiMessageList = (params: {
@@ -52,7 +52,7 @@ export const getApiMessageList = (params: {
       any,
       (
         | FcResponse<{  
-            services:Service[];
+            services: Service[];
         }>
         | undefined
       )
@@ -62,10 +62,103 @@ export const getApiMessageList = (params: {
       ...params
     });
   };
-  
 
+/**
+ * 通过上传 YAML 文本创建/编辑服务
+ * @returns
+ */
+export const uploadYAML = (params: {
+    serviceId?: string;
+    yaml: string;
+  }
+    ): Promise<
+      [
+        any,
+        (
+          | FcResponse<{  
+            serviceId: string;
+            name: string;
+            apis: ApiMessage[];
+          }>
+          | undefined
+        )
+      ]
+    > => {
+      return post('/api/service',{
+        ...params
+      });
+    };
+
+/**
+ * 获取指定服务的详细信息及 YAML 内容
+ * @returns
+ */
+export const getYAMLByServiceId = (params: {
+    serviceId: string;
+  }
+    ): Promise<
+      [
+        any,
+        (
+          | FcResponse<{  
+            serviceId: string;
+            name: string;
+            yaml: string;
+          }>
+          | undefined
+        )
+      ]
+    > => {
+      return get(`/api/service/${params.serviceId}`)
+    };
+
+/**
+ * 删除指定服务
+ * @returns
+ */
+export const deleteYAMLByServiceId = (params: {
+    serviceId: string;
+  }
+    ): Promise<
+      [
+        any,
+        (
+          | FcResponse<{  
+            serviceId: string;
+          }>
+          | undefined
+        )
+      ]
+    > => {
+      return del(`/api/service/${params.serviceId}`);
+    };
+    
+/**
+ * 更改服务收藏状态
+ * @returns
+ */
+export const likeServiceByServiceId = (params: {
+    serviceId: string;
+  }
+    ): Promise<
+      [
+        any,
+        (
+          | FcResponse<{  
+            serviceId: string;
+          }>
+          | undefined
+        )
+      ]
+    > => {
+      return put(`/api/service/${params.serviceId}`);
+    };
 
 export const apiApi = {
   getApiMessageByServiceId,
   getApiMessageList,
+  uploadYAML,
+  getYAMLByServiceId,
+  deleteYAMLByServiceId,
+  likeServiceByServiceId,
 };
