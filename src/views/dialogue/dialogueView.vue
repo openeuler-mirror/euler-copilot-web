bb87512cfce581de2e5f8a76a62e580981fad2b5
cc500 <2014434568@qq.com>
2025-02-18 15:21:01 +0800

    遗漏翻译字段

--- src/views/dialogue/dialogueView.vue
+++ src/views/dialogue/dialogueView.vue
@@ -19,6 +19,7 @@ import CopilotIconSelected from '@/assets/images/routerCopilotSelected.png';
 import WitchainDIcon from '@/assets/images/witchainD.png';
 import WitchainDIconSelected from '@/assets/svgs/WitchainDSelected.svg';
 import tools from '../tools/index.vue';
+import { successMsg } from 'src/components/Message';
 const { createNewSession } = useHistorySessionStore();
 
 // 挂载全局事件
@@ -144,6 +145,7 @@ const handleKnowledgeDialogClose = () => {
 
 const copy = () => {
   navigator.clipboard.writeText(apikey.value);
+  successMsg('复制成功');
 };
 
 const lang = computed(() => (language.value === 'EN' ? 'English' : '简体中文'));
