06eda7c47448820564ac086aa8cd8042b85f5848
李娜 <lnlnln629@163.com>
2025-03-05 03:30:43 +0000

    update src/views/dialogue/dialogueView.vue.
    Signed-off-by: 李娜 <lnlnln629@163.com>

--- src/views/dialogue/dialogueView.vue
+++ src/views/dialogue/dialogueView.vue
@@ -210,7 +210,6 @@ const changeLanguagefun = (lang: 'CN' | 'EN') => {
   if (iframe?.contentWindow) {
     const data = {lang:localStorage.getItem('localeLang')};
     let target = `${window.location.origin}/witchaind`;
-    // let target = `http://localhost:3002`;  // 本地调试
     iframe.contentWindow.postMessage(data, target);
   }
 };
@@ -231,7 +230,6 @@ onMounted(() => {
   console.log('iframe', `${window.location.origin}/witchaind`);
   initCopilot();
   iframe.src = `${window.location.origin}/witchaind`;
-  // iframe.src = `http://localhost:3002`;
 
 
 });
