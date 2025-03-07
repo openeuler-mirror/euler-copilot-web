ba18220446626e0a213da7129f42b20cda17430e
li-shengren-123456 <lishengren@h-partners.com>
2025-03-07 17:29:16 +0800

    引入语义接口相应图标

--- src/views/createapp/components/types.ts
+++ src/views/createapp/components/types.ts
@@ -9,6 +9,24 @@
 // See the Mulan PSL v2 for more details.
 import type { UserDialoguePanelType, RobotDialoguePanelType } from 'src/components/dialoguePanel/type';
 
+// 引入图片--系统下相关的图标
+import KENOWLEDGE_BASE from '@/assets/svgs/knowledgeBase.svg';
+import LLM from '@/assets/svgs/LLM.svg';
+import CONDITION from '@/assets/svgs/condition.svg';
+import LOOP from '@/assets/svgs/loop.svg';
+import TEMPLATE_CONVERSION from '@/assets/svgs/templateConversion.svg';
+// 引入图片--Aops-apollo相关图标
+import QUERY from '@/assets/svgs/query.svg';
+import get_CVE_DETAIL from '@/assets/svgs/getCveDetail.svg';
+// 引入图片--Euler-Copilot-tune相关图标
+import GATHER_METRICS from '@/assets/svgs/gatherMetrics.svg';
+// 引入图片--其他相关图标
+import API from '@/assets/svgs/API.svg';
+import TASK_CHOICE from '@/assets/svgs/taskChoice.svg';
+// 引入图片--用户自上传相关图标
+import USER_CODE from '@/assets/svgs/userCode.svg';
+import USER_DATABASE_CLASS from '@/assets/svgs/userDatabaseClass.svg';
+import USER_DOCUMENT_CLASS from '@/assets/svgs/userDatabaseClass.svg';
 // 工具类型
 export type LinkType = 'redirect' | 'action';
 
@@ -24,6 +42,30 @@ export const StatusInfoTitle = {
   running: '运行中',
   pending: '运行中',
 };
+
+export const nodeTypeToIcon = {
+  // 系统相关图标
+  KENOWLEDGE_BASE,
+  LLM,
+  CONDITION,
+  LOOP,
+
+  // Aops-apollo相关
+  QUERY,
+  get_CVE_DETAIL,
+
+  // Euler-Copilot-tune相关图标
+  GATHER_METRICS,
+
+  // 其他相关图标
+  API,
+  TASK_CHOICE,
+
+  // 用户自上传相关图标
+  USER_CODE,
+  USER_DATABASE_CLASS,
+  USER_DOCUMENT_CLASS,
+}
 
 export interface LinkItem {
   key: string;
