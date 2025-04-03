0c6fb04206ed9d669a78455a95330a287ffe0de8
史鸿宇 <shywzt@iCloud.com>
2025-04-03 14:41:59 +0800

    chore: clean code
    Signed-off-by: 史鸿宇 <shywzt@iCloud.com>

--- src/main.ts
+++ src/main.ts
@@ -7,20 +7,20 @@
 // IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
 // PURPOSE.
 // See the Mulan PSL v2 for more details.
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-import ElementPlus from 'element-plus';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
 
-import 'element-plus/dist/index.css';
-import 'highlight.js/styles/github-dark.css';
-import 'src/assets/base.css';
-import 'src/assets/styles/main.scss';
-import 'src/assets/styles/element/index.scss';
-import opendesign2 from '@computing/opendesign2';
-import '@computing/opendesign2/themes/es/css';
+import "element-plus/dist/index.css";
+import "highlight.js/styles/github-dark.css";
+import "src/assets/base.css";
+import "src/assets/styles/main.scss";
+import "src/assets/styles/element/index.scss";
+import opendesign2 from "@computing/opendesign2";
+import "@computing/opendesign2/themes/es/css";
 
-import App from './App.vue';
-import router from './router';
+import App from "./App.vue";
+import router from "./router";
 
 const app = createApp(App);
 
@@ -29,4 +29,4 @@ app.use(router);
 app.use(ElementPlus);
 app.use(opendesign2);
 
-app.mount('#app');
+app.mount("#app");
