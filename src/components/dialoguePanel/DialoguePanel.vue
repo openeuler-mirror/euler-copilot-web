26eb67d9d3ae11e7211073b375b96021d3d19101
cc500 <2014434568@qq.com>
2025-04-22 16:07:08 +0800

    清楚冗余代码

--- src/components/dialoguePanel/DialoguePanel.vue
+++ src/components/dialoguePanel/DialoguePanel.vue
@@ -75,8 +75,6 @@ export interface DialoguePanelProps {
   modeOptions: any;
   // 新增是否是工作流调试的-用于修改调试抽屉样式
   isWorkFlowDebug: boolean;
-
-  changeCommentByIndex: (index: number) => void;
 }
 import JsonFormComponent from './JsonFormComponent.vue';
 import { Metadata } from 'src/apis/paths/type';
@@ -101,7 +99,7 @@ const props = withDefaults(defineProps<DialoguePanelProps>(), {
 const messageArray = ref<MessageArray>(props.messageArray);
 const thoughtContent = ref('');
 const index = ref(0);
-const isComment = ref(undefined);
+const isComment = ref("none");
 const emits = defineEmits<{
   (e: 'handleReport', qaRecordId: string, reason?: string): void;
   (
