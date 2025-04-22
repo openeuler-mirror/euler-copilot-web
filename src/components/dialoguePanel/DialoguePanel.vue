e10e1ff66fd0946f5590c175a1c90f4194ee1f0d
cc500 <2014434568@qq.com>
2025-04-22 16:04:21 +0800

    初始化点赞点踩数值

--- src/components/dialoguePanel/DialoguePanel.vue
+++ src/components/dialoguePanel/DialoguePanel.vue
@@ -128,7 +128,7 @@ const handlePauseAndReGenerate = (cid?: number) => {
     thoughtContent.value = '';
     reGenerateAnswer(cid, user_selected_app.value);
     index.value = messageArray.value.getAllItems().length - 1;
-    isComment.value = undefined;
+    isComment.value = "none";
   } else {
     // 停止生成
     pausedStream(cid);
@@ -395,6 +395,13 @@ watch(
     popperSize();
   },
 );
+
+watch(
+  () => index.value, 
+  () => {
+    handleIsLike();
+  }
+)
 
 onBeforeUnmount(() => {
   isComment.value = undefined;
