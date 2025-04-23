17412c8eb7528d2ddb6ab77211b1417a7a9a8ddd
cc500 <2014434568@qq.com>
2025-04-23 11:13:38 +0800

    更新点踩事件的处理逻辑

--- src/components/dialoguePanel/DialoguePanel.vue
+++ src/components/dialoguePanel/DialoguePanel.vue
@@ -198,7 +198,7 @@ const handleDislike = async (
       reasonDescription: reasonDescription,
     }
   ).then((res) => {
-    if(res[0].status === 200){
+    if(res[1].status === 200){
       isAgainstVisible.value = false;
       isAgainst.value = isAgainst.value ? false : true;
       isSupport.value = false;
