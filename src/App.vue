86e9be0e2e09e759fe852e6200429f646d7cf8c4
李娜 <lnlnln629@163.com>
2025-04-23 14:18:26 +0800

    fix:处理切换页面时轮询问题

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,32 @@ import { qiankunWindow } from 'vite-plugin-qiankun/dist/helper';
 import LinuxTitleBar from '@/components/LinuxTitleBar.vue';
 import '@vue-flow/core/dist/style.css';
 import '@vue-flow/core/dist/theme-default.css';
+import DialogueView from 'src/views/dialogue/dialogueView.vue';
+import { watch } from 'vue';
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+// 监听路由变化，控制iframe内容的活动状态
+watch(
+  () => router.currentRoute.value.path,
+  (newPath) => {
+    const iframe = document.getElementById('my-iframe');
+    const isWitchaindRoute = newPath === '/witchainD';
+    if (!iframe) {
+      console.warn('未找到iframe元素');
+      return;
+    }
+    if (!iframe.contentWindow) {
+      console.warn('iframe.contentWindow不可用');
+      return;
+    }
+    const message = { StopActive: !isWitchaindRoute };
+    // 向iframe发送消息，控制其活动状态 StopActive为false表示激活，为true表示停止
+    let target = `${window.location.origin}/witchaind`;
+    iframe.contentWindow.postMessage(message, target);
+  },
+  { immediate: true } // 初始化时也执行
+);
 
 // 平台检测
 const isLinuxPlatform = ref(false);
@@ -17,6 +43,8 @@ onMounted(() => {
 </script>
 
 <template>
+  <div class="eulercopilot-main" :style="{ height: qiankunWindow.__POWERED_BY_QIANKUN__ ? '100%' : '100vh' }">
+    <DialogueView />
   <div
     class="eulercopilot-main"
     :style="{ height: qiankunWindow.__POWERED_BY_QIANKUN__ ? '100%' : '100vh' }"
@@ -25,6 +53,7 @@ onMounted(() => {
     <LinuxTitleBar v-if="isLinuxPlatform" />
 
     <router-view />
+    </div>
   </div>
 </template>
 
