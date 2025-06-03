5ccc68ea76c27499d25f39477047620d309e900c
cc500 <2014434568@qq.com>
2025-06-03 11:39:11 +0800

    initalPanel除图标外修改

--- src/views/dialogue/components/InitalPanel.vue
+++ src/views/dialogue/components/InitalPanel.vue
@@ -83,7 +83,7 @@ onMounted(() => {
       <p class="inital-panel-tips"></p>
       <div class="container">
         <div class="eg">
-          <p>{{ $t('main.left_describe') }}</p>
+          <p class="eg-title">{{ $t('main.left_describe') }}</p>
           <ul class="eg-list">
             <li
               class="eg-list-item"
@@ -182,18 +182,23 @@ onMounted(() => {
 
 .question-list {
   margin-top: 16px;
+  font-size: 16px;
   li {
     margin-bottom: 16px;
     .blue {
-      color: #5ab3ff;
+      background: linear-gradient(to bottom, rgb(109, 117, 250), rgb(90, 179, 255));
+      -webkit-background-clip: text;
+      background-clip: text;
+      -webkit-text-fill-color: transparent;
     }
   }
   .question-number {
+    font-size: 14px;
     margin-right: 8px;
     color: var(--o-text-color-secondary);
   }
   .question-des {
-    color: var(--o-text-color-secondary);
+    color: var(--o-text-color-primary);
   }
 }
 
@@ -264,7 +269,7 @@ onMounted(() => {
     height: 36px;
     justify-content: center;
     line-height: 36px;
-    font-weight: 500;
+    font-weight: 700;
     font-size: 28px;
     color: var(--o-text-color-primary);
   }
@@ -286,9 +291,10 @@ onMounted(() => {
     border-radius: 8px;
     width: 492px;
     height: 320px;
-    p {
-      font-size: 16px;
-      color: var(--o-text-color-secondary);
+    &-title {
+      font-size: 18px;
+      font-weight: 700;
+      color: var(--o-text-color-primary);
     }
     &-btn {
       display: flex;
@@ -331,7 +337,7 @@ onMounted(() => {
       &-item {
         display: flex;
         align-items: center;
-        width: calc(50% - 8px);
+        width: 218px;
         height: 80px;
         background: var(--o-bg-color-base);
         overflow: hidden;
@@ -344,11 +350,12 @@ onMounted(() => {
         img {
           width: 37px;
           height: 37px;
-          margin: 0 6px 0 8px;
+          margin: 16px 8px 32px 16px;
         }
 
         &__text {
-          width: 100%;
+          // width: 100%;
+          margin-right: 16px;
           display: flex;
           flex-direction: column;
           font-size: 16px;
@@ -357,7 +364,7 @@ onMounted(() => {
           line-height: 24px;
 
           &-desc {
-            font-size: 12px;
+            font-size: 14px;
             color: var(--o-text-color-secondary);
             font-weight: 400;
           }
