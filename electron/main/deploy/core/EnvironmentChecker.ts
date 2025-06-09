3599f9e602a00f9a9ba0251c70c3ea9ba7e1dad3
史鸿宇 <shywzt@iCloud.com>
2025-06-09 15:35:23 +0800

    chore: 更新文件版权信息和许可证条款说明
    Signed-off-by: 史鸿宇 <shywzt@iCloud.com>

--- electron/main/deploy/core/EnvironmentChecker.ts
+++ electron/main/deploy/core/EnvironmentChecker.ts
@@ -1,5 +1,12 @@
 // Copyright (c) Huawei Technologies Co., Ltd. 2023-2025. All rights reserved.
 // licensed under the Mulan PSL v2.
+// You can use this software according to the terms and conditions of the Mulan PSL v2.
+// You may obtain a copy of Mulan PSL v2 at:
+//      http://license.coscl.org.cn/MulanPSL2
+// THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+// PURPOSE.
+// See the Mulan PSL v2 for more details.
 
 import * as os from 'os';
 import * as fs from 'fs';
@@ -189,7 +196,7 @@ export class EnvironmentChecker {
           timeout: 10000,
         },
       );
-    } catch (error) {
+    } catch {
       throw new Error('无法访问外部网络，请检查网络连接');
     }
   }
@@ -204,7 +211,7 @@ export class EnvironmentChecker {
     for (const tool of requiredTools) {
       try {
         await execAsync(`which ${tool}`);
-      } catch (error) {
+      } catch {
         missingTools.push(tool);
       }
     }
